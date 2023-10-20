a6815448a592b5d9b67984dc75c5f0b87da4df6c
Oscar Jimmy Marquez <ojmg69@gmail.com>
2023-10-20 02:40:52 -0400

    corrigiendo feha de grafico ventas

--- resources/js/Pages/Reporte/Ventas.vue
+++ resources/js/Pages/Reporte/Ventas.vue
@@ -11,11 +11,10 @@ import moment from 'moment';
 import 'vue-datepicker-next/locale/es.es.js';
 import VueApexCharts from "vue3-apexcharts";
 
-const datos_ventas = ref([]);
-const { datos_grafico_ventas } = usePage().props
 const date = ref();
+const chart1 = ref();
 const titulo = "Reporte Ventas"
-const ruta = 'reportes'
+
 //filtrado
 const filtrado = (value) => {
     if (value[0] != null && value[1] != null) {
@@ -28,151 +27,35 @@ const filtrado = (value) => {
             {
                 preserveState: true,
                 onSuccess: () => {
-                    grafVentas.value.series[0].data = [];
-                    grafVentas.value.chartOptions.labels = [];
-                    grafVentas.value.series[0].data = usePage().props.datos_grafico_ventas.datos;
-                    grafVentas.value.chartOptions.labels = usePage().props.datos_grafico_ventas.categoria;
+                    chart1.value.updateSeries([{
+                        data: usePage().props.datos_grafico_ventas.datos
+                    }])
+
+                    chart1.value.updateOptions({
+
+                        labels: usePage().props.datos_grafico_ventas.categoria,
+
+                    })
+
                 }
             }
         );
-        date.value =[ moment(value[0]).format('YYYY-MM-DD'), moment(value[1]).format('YYYY-MM-DD')];
+        date.value = [moment(value[0]).format('YYYY-MM-DD'), moment(value[1]).format('YYYY-MM-DD')];
     }
 }
 onMounted(() => {
     date.value = [subDays(new Date(), 30), new Date()];
     filtrado(date.value);
-    grafVentas.value.series[0].data = [];
-    grafVentas.value.chartOptions.labels = [];
-    grafVentas.value.series[0].data = usePage().props.datos_grafico_ventas.datos;
-    grafVentas.value.chartOptions.labels = usePage().props.datos_grafico_ventas.categoria;
+
 
 });
+
 //grafico total ventas
-
-const grafVentas = ref({
-    series: [{
-        name: 'VENTAS',
-        type: 'area',
-        data: []
-    }],
-
-    chartOptions: {
-        chart: {
-            height: 350,
-            type: 'area',
-            stacked: false,
-            toolbar: {
-                show: true,
-                offsetX: 0,
-                offsetY: 0,
-                tools: {
-                    download: true,
-                    selection: false,
-                    zoom: false,
-                    zoomin: false,
-                    zoomout: false,
-                    pan: false,
-                    reset: false,
-                    customIcons: []
-                },
-                export: {
-                    csv: {
-                        filename: "ventas",
-                        columnDelimiter: ',',
-                        headerCategory: 'anio',
-                        headerValue: 'value',
-                        dateFormatter(timestamp) {
-                            return new Date(timestamp).toDateString()
-                        }
-                    },
-                    svg: {
-                        filename: "ventas",
-                    },
-                    png: {
-                        filename: "ventas",
-                    }
-                },
-            },
-        },
-        stroke: {
-            width: [2, 2],
-            curve: 'straight'
-
-        },
-        responsive: [
-            {
-                breakpoint: 480,
-                options: {
-                    chart: {
-                        width: 100
-                    },
-                    legend: {
-                        position: "bottom"
-                    }
-                }
-            }
-        ],
-        plotOptions: {
-            bar: {
-                columnWidth: '50%'
-            }
-        },
-
-        fill: {
-            opacity: [0.85, 0.25, 1],
-            gradient: {
-                inverseColors: false,
-                shade: 'light',
-                type: "vertical",
-                opacityFrom: 0.85,
-                opacityTo: 0.55,
-                stops: [0, 100, 100, 100]
-            }
-        },
-        labels: [],
-        markers: {
-            size: 3,
-            strokeWidth: 2
-        },
-        xaxis: {
-            //type: 'datetime'
-            title: {
-                    text: "VENTAS",
-                    style: {
-                        fontSize:16
-                        //color: "#0EA5E9"
-                    }
-                }
-        },
-        yaxis: [
-            {
-                axisTicks: {
-                    show: false
-                },
-                axisBorder: {
-                    show: true,
-                    //  color: "#0EA5E9"
-                },
-
-                labels: {
-                    formatter: function (val) {
-                        return "$ " + val.toFixed(2)
-                    }
-                },
-                title: {
-                    text: "Ventas",
-                    style: {
-                        //color: "#0EA5E9"
-                    }
-                }
-            }
-        ],
-
-    }
-})
-const totalVentasLinea = ref({
+const series = ref([]);
+const chartOptions = ref({
     chart: {
-        type: "area",
+        height: 350,
+        type: 'area',
         stacked: false,
         toolbar: {
             show: true,
@@ -207,30 +90,54 @@ const totalVentasLinea = ref({
             },
         },
     },
-    dataLabels: {
-        enabled: false,
-        formatter: function (val) {
-            return "$ " + val.toFixed(2)
-        },
-        offsetY: -6,
-        style: {
-            fontSize: '10px',
-            colors: ["#304758"]
+    stroke: {
+        width: [2, 2],
+        curve: 'straight'
+
+    },
+    responsive: [
+        {
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 100
+                },
+                legend: {
+                    position: "bottom"
+                }
+            }
+        }
+    ],
+    plotOptions: {
+        bar: {
+            columnWidth: '50%'
         }
     },
+
+    fill: {
+        opacity: [0.85, 0.25, 1],
+        gradient: {
+            inverseColors: false,
+            shade: 'light',
+            type: "vertical",
+            opacityFrom: 0.85,
+            opacityTo: 0.55,
+            stops: [0, 100, 100, 100]
+        }
+    },
+    labels: [],
     markers: {
         size: 3,
         strokeWidth: 2
     },
-
-
-
     xaxis: {
-        categories: datos_ventas.categoria
-    },
-    plotOptions: {
-        bar: {
-            horizontal: false
+        //type: 'datetime'
+        title: {
+            text: "VENTAS",
+            style: {
+                fontSize: 16
+                //color: "#0EA5E9"
+            }
         }
     },
     yaxis: [
@@ -257,12 +164,9 @@ const totalVentasLinea = ref({
         }
     ],
 
-    legend: {
-        horizontalAlign: "botton",
-        offsetX: 20
-    },
+}
+)
 
-})
 //*datepicker  */
 const shortcuts = [
     {
@@ -305,8 +209,7 @@ const shortcuts = [
 
 <template>
     <Head title="Reporte Ventas" />
-    <AppLayout
-    :pagina="[{ 'label': 'Reportes', link: false }, { 'label': titulo, link: false }]">
+    <AppLayout :pagina="[{ 'label': 'Reportes', link: false }, { 'label': titulo, link: false }]">
 
         <div class="card px-4 mb-4 col-span-12 rounded-lg">
             <div class="col-span-full flex justify-between items-center">
@@ -327,29 +230,11 @@ const shortcuts = [
                     <h3 class="flex items-center mb-4 text-lg font-semibold text-gray-900">Gráfico de Ventas
                     </h3>
                     <div id="chart">
-                        <VueApexCharts type="area" height="350" class="w-auto" :options="grafVentas.chartOptions"
-                            :series="grafVentas.series">
+                        <VueApexCharts ref="chart1" type="area" height="350" class="w-auto" :options="chartOptions"
+                            :series="series">
                         </VueApexCharts>
                     </div>
                 </div>
-                <!--
-
-                    <Link :href="route('ventas.index')" method="get" as="button"   v-if="permissions.includes('lista-ventas')"
-                    class="p-4 col-span-12 lg:col-span-3  rounded-lg bg-primary-900 shadow-lg cursor-pointer  hover:bg-primary-100 dark:hover:bg-gray-600 dark:bg-gray-700">
-                    <div class="flex justify-center text-white items-center p-2 mx-auto mb-2">
-                        <i class="fa fa-shopping-cart fa-2x"></i>
-                    </div>
-                    <div class="font-medium text-center text-white dark:text-gray-400">Ventas</div>
-                </Link>
-
-                <Link :href="route('usuarios.index')" method="get" as="button"  v-if="permissions.includes('importaciones')"
-                class="p-4 col-span-12 lg:col-span-3  rounded-lg bg-primary-900 shadow-lg cursor-pointer  hover:bg-primary-100 dark:hover:bg-gray-600 dark:bg-gray-700">
-                <div class="flex justify-center text-white items-center p-2 mx-auto mb-2">
-                    <i class="fa fa-ship icon fa-2x"></i>
-                </div>
-                <div class="font-medium text-center text-white dark:text-gray-400">Importaciones</div>
-            </Link>
-        -->
 
                 <!--Contenido-->
             </div>
