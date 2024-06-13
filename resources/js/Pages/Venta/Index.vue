05795b67c5e524798bad9abed957e05def8dbc95
Oscar Jimmy Marquez Gomez <ojmg69@gmail.com>
2024-06-13 19:31:29 +0000

    Revert "Merge branch 'develop' into testing"
    This reverts commit 41c9c5bacd94d4c46008df9fa8a60b5111aef4d2

--- resources/js/Pages/Venta/Index.vue
+++ resources/js/Pages/Venta/Index.vue
@@ -1,9 +1,10 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import { ref, onMounted,watch } from 'vue'
+import { ref, onMounted } from 'vue'
 import { Head, usePage, useForm, router } from '@inertiajs/vue3';
 import Swal from 'sweetalert2';
 import { FilterMatchMode } from 'primevue/api';
+import Column from 'primevue/column';
 import Button from 'primevue/button';
 import { useToast } from "primevue/usetoast";
 
@@ -12,10 +13,9 @@ import 'vue-datepicker-next/index.css';
 import { endOfMonth, endOfYear, startOfMonth, subDays, startOfYear } from 'date-fns';
 import moment from 'moment';
 import 'vue-datepicker-next/locale/es.es.js';
-import Pagination from '@/Components/Pagination.vue';
-const props = defineProps(['ventas'])
-const toast = useToast();
 
+const toast = useToast();
+const tabla_ventas = ref()
 const cargando = ref(false)
 const { permissions } = usePage().props.auth
 const titulo = "Historial de Ventas"
@@ -60,23 +60,16 @@ const shortcuts = [
         },
     },
 ]
-const searchField = ref(''); //Should really load it from the query string
-
-const url = route('ventas.index');
-
-watch(searchField, 
-() => {
-	router.get(url, {buscar: searchField.value}, {preserveState: true, preserveScroll: true, only: ['ventas']}), 300}
-    );
 
 onMounted(() => {
     date.value = [subDays(new Date(), 2), new Date()];
-    //filtrado(date.value);
+    filtrado(date.value);
 });
 
 //filtrado
 const filtrado = (value) => {
     if (value[0] != null && value[1] != null) {
+        tabla_ventas.value = [];
         cargando.value = true;
         router.get('/ventas/',
             {
@@ -86,7 +79,8 @@ const filtrado = (value) => {
             {
                 preserveState: true,
                 onSuccess: () => {
-                    
+                    //tabla_ventas.value = Array.from(usePage().props.ventas.data, (x) => x);
+                    tabla_ventas.value = usePage().props.ventas.data;
                     cargando.value = false;
                 }
 
@@ -94,7 +88,7 @@ const filtrado = (value) => {
         );
         date.value = [moment(value[0]).format('YYYY-MM-DD'), moment(value[1]).format('YYYY-MM-DD')];
     } else {
-        //router.get(route(ruta + '.index'))
+        router.get(route(ruta + '.index'))
     }
 
 }
@@ -156,8 +150,9 @@ const btnEliminar = (id) => {
     });
 }
 
-const clickDetalle = (id) => {
-    btnVer(id)
+const clickDetalle = (e) => {
+
+    btnVer(e.data.id)
 }
 
 
@@ -188,10 +183,10 @@ const filters = ref({
 });
 </script>
 <template>
-
     <Head :title="titulo" />
     <AppLayout :pagina="[{ 'label': titulo, link: false }]">
-        <div class="card p-4 mb-4 bg-white col-span-12  rounded-lg shadow-lg 2xl:col-span-12">
+        <div class="card px-4 mb-4 bg-white col-span-12  rounded-lg shadow-lg 2xl:col-span-12">
+
             <!--Contenido-->
             <Toast />
             <div class="px-3 pb-2 col-span-full flex justify-between items-center">
@@ -199,63 +194,170 @@ const filters = ref({
                 <Button size="small" :label="'Crear Venta'" severity="success" @click="BtnCrear"></Button>
 
             </div>
-            <!--tabla-->
-            <div class="align-middle py-4">
-                <div class="grid grid-cols-6 gap-4 m-1.5">
-                    <InputText v-model="searchField" placeholder="Buscar N° Compra" :pt="{
-                        root: { class: 'col-span-6 lg:col-span-2 m-1.5' }
-                    }" />
 
-                    <date-picker @change="filtrado" type="date" range value-type="YYYY-MM-DD" format="DD/MM/YYYY"
-                        class="p-inputtext p-component col-span-6 lg:col-span-2 font-sans  font-normal text-gray-700  bg-white  transition-colors duration-200 border-0 text-sm"
-                        v-model:value="date" :shortcuts="shortcuts" lang="es"
-                        placeholder="Seleccione Fecha"></date-picker>
+            <div class="align-middle">
 
-                </div>
-                <div style="overflow:auto; max-height: 700px;">
+                <DataTable :filters="filters" :value="tabla_ventas" :loading="cargando" :pt="{
+                    bodyRow: { class: 'hover:cursor-pointer hover:bg-gray-100' }
+                }" scrollable scrollHeight="700px" paginator :rows="50" @row-click="clickDetalle"
+                    size="small">
 
-                    <table class="w-full text-md bg-white shadow-md rounded mb-4">
-                        <thead style="position: sticky;" class="top-0 z-[1]">
-                            <tr class="bg-secondary-100">
-                                <th class="p-1.5">Fecha y Hora</th>
-                                <th>Nº Compra</th>
-                                <th>Cliente</th>
-                                <th>Estado</th>
-                                <th>Total</th>
-                                <th>Observaciones</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
+                    <template #header>
+                        <div class="grid grid-cols-6 gap-4 m-1.5">
+                            <InputText v-model="filters['global'].value" placeholder="Buscar" :pt="{
+                                root: { class: 'col-span-6 lg:col-span-2 m-1.5' }
+                            }" />
 
-                        <tbody>
-                            <tr v-for="post in props.ventas.data" 
-                                class="border text-center hover:cursor-pointer hover:bg-gray-100">
-                                <td @click="clickDetalle(post.id)">{{ post.fecha }}</td>
-                                <td @click="clickDetalle(post.id)">{{ post.nro_compra }}</td>
-                                <td @click="clickDetalle(post.id)">{{ JSON.parse(post.cliente)["nombre"]??""}}</td>
-                                <td @click="clickDetalle(post.id)" class="p-1.5"> <span class="font-semibold text-md"
-                                        :class="colorEstado(post.estado)">
-                                        {{ post.estado }}
-                                    </span></td>
-                                <td>{{ post.total }}</td>
-                                <td @click="clickDetalle(post.id)">{{ post.observaciones }}</td>
-                                <td>
-                                    <Button v-if="permissions.includes('eliminar-ventas') && post.estado !== 'ANULADO'"
-                                        @click="btnEliminar(post.id)"
-                                        class="w-8 h-8 rounded border-red-700 bg-red-700 px-2 py-1 text-base font-normal text-white m-1 hover:bg-red-600 "
-                                        v-tooltip.top="{ value: `Anular`, pt: { text: 'bg-gray-500 p-1 text-xs text-white rounded' } }"><i
-                                            class="fas fa-ban"></i></Button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                            <date-picker @change="filtrado" type="date" range value-type="YYYY-MM-DD" format="DD/MM/YYYY"
+                                class="p-inputtext p-component col-span-6 lg:col-span-2 font-sans  font-normal text-gray-700  bg-white  transition-colors duration-200 border-0 text-sm"
+                                v-model:value="date" :shortcuts="shortcuts" lang="es"
+                                placeholder="Seleccione Fecha"></date-picker>
 
-                <Pagination :elements="props.ventas"></Pagination>
+                        </div>
+                    </template>
+
+                    <template #empty> No existe Resultado </template>
+                    <template #loading> Cargando... </template>
+                    <Column field="fecha" header="Fecha y Hora" sortable :pt="{
+                        bodyCellContent: {
+                            class: 'text-center w-44'
+                        },
+                        headerContent: {
+
+                            class: 'text-center w-44'
+                        },
+                        headerCell: {
+
+                            class: 'text-center w-44'
+                        },
+                        bodyCell: {
+
+                            class: 'text-center'
+                        }
+                    }"> <template #loading>
+                        </template></Column>
+                    <Column field="nro_compra" header="Nº Compra" context="small" sortable :pt="{
+
+                        bodyCellContent: {
+                            class: ' w-44'
+                        },
+                        headerCell: {
+                            class: 'w-44'
+                        },
+                        bodyCell: {
+
+                            class: 'text-center'
+                        },
+                        headerContent: {
+
+                            class: 'text-center w-44'
+                        },
+
+                    }">
+                        <template #loading>
+                        </template>
+                    </Column>
+
+                    <Column field="cliente" header="Cliente" sortable :pt="{
+                        bodyCellContent: {
+                            class: 'text-center w-56'
+                        },
+                        headerContent: {
+
+                            class: 'text-center w-56'
+                        },
+                        headerCell: {
+
+                            class: 'text-center w-56'
+                        },
+                        bodyCell: {
+
+                            class: 'text-center'
+                        }
+                    }">
+                        <template #loading>
+                            <div class="flex align-items-center"
+                                :style="{ height: '17px', 'flex-grow': '1', overflow: 'hidden' }">
+
+                            </div>
+                        </template>
+                    </Column>
+
+                    <Column field="estado" header="Estado" sortable :pt="{
+                        bodyCellContent: {
+                            class: 'text-center w-52'
+                        },
+                        headerContent: {
+
+                            class: 'text-center w-52'
+                        },
+                        headerCell: {
+
+                            class: 'text-center w-52'
+                        },
+                        bodyCell: {
+
+                            class: 'text-center'
+                        },
+                    }">
+                        <template #loading>
+                        </template>
+                        <template #body="slotProps">
+                            <span class="font-semibold text-md" :class="colorEstado(slotProps.data.estado)">
+                                {{ slotProps.data.estado }}
+                            </span>
+                        </template>
+                    </Column>
+
+                    <Column field="total" sortable header="Total" :pt="{
+                        bodyCellContent: {
+                            class: 'text-center w-36'
+                        },
+                        headerContent: {
+
+                            class: 'text-center w-36'
+                        },
+                        headerCell: {
+
+                            class: 'text-center w-36'
+                        },
+                        bodyCell: {
+
+                            class: 'text-end'
+                        }
+                    }"></Column>
+
+                    <Column field="observaciones" sortable header="Observaciones" :pt="{
+                        bodyCellContent: {
+                            class: 'text-center w-52'
+                        },
+                        bodyCell: {
+
+                            class: 'text-center'
+                        },
+                    }"></Column>
+
+                    <Column header="Acciones" style="width:100px" :pt="{
+                        bodyCell: {
+                            class: 'text-center p-0'
+                        }
+                    }">
+                        <template #loading>
+                        </template>
+                        <template #body="slotProps">
+
+                            <Button v-if="permissions.includes('eliminar-ventas') && slotProps.data.estado !== 'ANULADO'"
+                                @click="btnEliminar(slotProps.data.id)"
+                                class="w-8 h-8 rounded border-red-700 bg-red-700 px-2 py-1 text-base font-normal text-white m-1 hover:bg-red-600 "
+                                v-tooltip.top="{ value: `Anular`, pt: { text: 'bg-gray-500 p-1 text-xs text-white rounded' } }"><i
+                                    class="fas fa-ban"></i></Button>
+
+
+                        </template>
+                    </Column>
+                </DataTable>
 
             </div>
-            <!--tabla-->
-
             <!--Contenido-->
         </div>
     </AppLayout>
